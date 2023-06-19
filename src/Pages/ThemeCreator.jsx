dc8fe12c0af898af760e1979626fda98dfebded7
Ninjaondra321 <ondran321@gmail.com>
2023-06-19 21:43:42 +0200

    Initial commit

--- src/Pages/ThemeCreator.jsx
+++
@@ -1,8 +0,0 @@
-
-function ThemeCreator({ language }) {
-    return (<div className="sections">
-        <h1>ThemeCreator</h1>
-    </div>);
-}
-
-export default ThemeCreator;
--- src/Pages/page404.jsx
+++
@@ -1,8 +0,0 @@
-
-function Page404({ language }) {
-    return (<div className="sections">
-        <h1>404</h1>
-    </div>);
-}
-
-export default Page404;
